bf0c2c239904265c5bd9aa9753a0a5efe5050861
Marlon Tineo <marlonjjtp@gmail.com>
2015-01-22 21:23:33 -0500

    fix on tracks/models.py

--- sfotipy/tracks/models.py
+++ sfotipy/tracks/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+from artists.models import Artist
+from albums.models import Album
+
 class Track(models.Model):
 	title = models.CharField(max_length=255)
 	order = models.PositiveIntegerField()
 	track_file = models.FileField(upload_to='tracks')
+	artist = models.ForeignKeyField(Artist)
+	album = models.ForeignKeyField(Album)
 
 	def __unicode__(self):
 		return self.title
